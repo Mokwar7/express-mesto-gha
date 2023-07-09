6d2d5f4e25a180174e3d4a0e2085a501fc6db640
Mokwar <mokwar7@gmail.com>
2023-07-09 23:46:56 +0300

    Eslint fix

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 const express = require('express');
 
 const mongoose = require('mongoose');
+
 const { PORT = 3000, BASE_PATH } = process.env;
+
 const app = express();
 
 mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
-    useNewUrlParser: true,
-  })
+  useNewUrlParser: true,
+})
   .then((res) => {
     console.log(res);
   })
   .catch((err) => {
     console.log(err);
-});
+  });
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '64a98ecce59ba66fded06755'
-  }
+    _id: '64a98ecce59ba66fded06755',
+  };
   next();
-})
+});
 
 app.use(express.json());
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
 app.use('*', (req, res) => {
-  res.status(404).send({message: 'Такой страницы не существует.'});
+  res.status(404).send({ message: 'Такой страницы не существует.' });
 });
 
 app.listen(PORT, () => {
   console.log(`Ссылка на сервер: ${PORT}`);
-  console.log(BASE_PATH ? BASE_PATH : 'BASE_PATH не определен');
+  console.log(BASE_PATH);
 });
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,23 +1,29 @@
 const Cards = require('../models/card');
-const { NOT_CORRECT_DATA_ERROR_CODE, NOT_FIND_ERROR_CODE, DEFAULT_ERROR_CODE, SUCCESS_CODE, CREATE_CODE, } = require('../utils/errorCodes');
+const {
+  NOT_CORRECT_DATA_ERROR_CODE,
+  NOT_FIND_ERROR_CODE,
+  DEFAULT_ERROR_CODE,
+  SUCCESS_CODE,
+  CREATE_CODE
+ } = require('../utils/errorCodes');
 
 const checkErr = (err, res) => {
   if (err.name === "CastError" || err.name === "ValidationError") {
-    res.status(NOT_CORRECT_DATA_ERROR_CODE).send({message: `Data validation error: ${err.message}`});
+    res.status(NOT_CORRECT_DATA_ERROR_CODE).send({ message: `Data validation error: ${err.message}` });
     return;
   }
   if (err.name === "DocumentNotFoundError") {
-    res.status(NOT_FIND_ERROR_CODE).send({message: `Invalid ID: ${err.message}`});
+    res.status(NOT_FIND_ERROR_CODE).send({ message: `Invalid ID: ${err.message}` });
     return;
   }
 
-  res.status(DEFAULT_ERROR_CODE).send({message: `Server error: ${err.message}`});
+  res.status(DEFAULT_ERROR_CODE).send({ message: `Server error: ${err.message}` });
 };
 
 module.exports.getAllCards = (req, res) => {
   Cards.find({})
     .then((cards) => {
-      res.status(SUCCESS_CODE).send({data: cards});
+      res.status(SUCCESS_CODE).send({ data: cards });
     })
     .catch(err => checkErr(err, res));
 };
@@ -28,7 +34,7 @@ module.exports.createCard = (req, res) => {
 
   Cards.create({name, link, owner: creatorId})
     .then((card) => {
-      res.status(CREATE_CODE).send({data: card});
+      res.status(CREATE_CODE).send({ data: card });
     })
     .catch(err => checkErr(err, res));
 };
@@ -39,7 +45,7 @@ module.exports.deleteCard = (req, res) => {
   Cards.findByIdAndRemove(cardId)
     .orFail()
     .then((result) => {
-      res.status(SUCCESS_CODE).send({data: result})
+      res.status(SUCCESS_CODE).send({ data: result })
     })
     .catch(err => checkErr(err, res));
 };
@@ -53,7 +59,7 @@ module.exports.putLike = (req, res) => {
   })
     .orFail()
     .then((result) => {
-      res.status(SUCCESS_CODE).send({data: result})
+      res.status(SUCCESS_CODE).send({ data: result })
     })
     .catch(err => checkErr(err, res));
 };
@@ -62,12 +68,12 @@ module.exports.deleteLike = (req, res) => {
   const { cardId } = req.params;
   const creatorId = req.user._id;
 
-  Cards.findByIdAndUpdate(cardId, { $pull: {likes: creatorId }}, {
+  Cards.findByIdAndUpdate(cardId, { $pull: { likes: creatorId }}, {
     new: true,
   })
     .orFail()
     .then((result) => {
-      res.status(SUCCESS_CODE).send({data: result})
+      res.status(SUCCESS_CODE).send({ data: result })
     })
     .catch(err => checkErr(err, res));
 };
