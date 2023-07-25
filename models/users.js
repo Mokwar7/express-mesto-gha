d50f6a4101892362dd73365d8fa56a99cd789e98
Mokwar <mokwar@MacBook-Air-Mokwar.local>
2023-07-25 15:29:22 +0300

    bug fix

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
 
-const NotCorrectDataError = require('../utils/notCorrectDataError');
+const NotCorrectTokenError = require('../utils/notCorrectTokenError');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -49,13 +49,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new NotCorrectDataError('Неправильные почта или пароль'));
+        return Promise.reject(new NotCorrectTokenError('Неправильные почта или пароль'));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new NotCorrectDataError('Неправильные почта или пароль'));
+            return Promise.reject(new NotCorrectTokenError('Неправильные почта или пароль'));
           }
           return user;
         });
